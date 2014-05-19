109a5e1869a9e005fb43aeee8d242eb8280db689
TJ <evertang@gmail.com>
2014-05-19 19:01:55 -0400

    saving to ycsb db

--- mstat/config.js
+++ mstat/config.js
@@ -1,7 +1,7 @@
 // configuration file
 
 module.exports= {
-	mongoUrl:'mongodb://tja1.mongodb.ga:27017/tjlabs',
+	mongoUrl:'mongodb://tja1.mongodb.ga:27017/ycsb',
 	interval: "5", // 5 seconds
 	servers: [
 		'tjs1.mongodb.ga:26000','tjs1.mongodb.ga:27017'
